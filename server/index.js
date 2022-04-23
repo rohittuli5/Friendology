32125dce0191c71d9537e848ff7df47f251a640b
Rohit Tuli <rohittuli08@gmail.com>
2022-04-23 19:40:24 +0530

    Initial deployment

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require('path');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
+app.get("/app", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
