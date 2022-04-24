737f11e3a93aa2bdbee2cfa51b57d270982b5b25
Sarvesh Vhawal <sarveshvhawal.007@gmail.com>
2022-04-24 11:44:28 +0530

    dummy questions added frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,63 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import {useState} from 'react';
+import './quiz.css';
+import questions from './question'
 function App() {
-  const [data, setData] = React.useState(null);
+  // const [data, setData] = React.useState(null);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
+  
+
+  // React.useEffect(() => {
+  //   fetch("/api")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data.message));
+  // }, []);
 
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
-      </header>
-    </div>
+const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
+	return (
+    <div>
+    <h1>Personality Test</h1>
+		<div className='app'>
+
+			{showScore ? (
+        <div className='score-section'>
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+        <>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+              <button onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</button>
+              ))}
+					</div>
+				</>
+			)}
+		</div>
+  </div>
   );
 
 }
---
+++ frontend/src/question.js
@@ -0,0 +1,95 @@
+const questions = [
+		{
+			questionText: 'DO you think you are dumb?',
+			answerOptions: [
+				{ answerText: 'Definetly', isCorrect: false,rating:0 },
+				{ answerText: 'I think Soo', isCorrect: false,rating:1 },
+				{ answerText: "NO, I don't think soo", isCorrect: true,rating:2 },
+				{ answerText: 'NO, fuck you', isCorrect: false,rating:3 },
+			],
+		},
+		{
+			questionText: 'DO you think you are dumb?',
+			answerOptions: [
+				{ answerText: 'Definetly', isCorrect: false,rating:0 },
+				{ answerText: 'I think Soo', isCorrect: false,rating:1 },
+				{ answerText: "NO, I don't think soo", isCorrect: true,rating:2 },
+				{ answerText: 'NO, fuck you', isCorrect: false,rating:3 },
+			],
+		},
+		{
+			questionText: 'DO you think you are dumb?',
+			answerOptions: [
+				{ answerText: 'Definetly', isCorrect: false,rating:0 },
+				{ answerText: 'I think Soo', isCorrect: false,rating:1 },
+				{ answerText: "NO, I don't think soo", isCorrect: true,rating:2 },
+				{ answerText: 'NO, fuck you', isCorrect: false,rating:3 },
+			],
+		},
+		{
+			questionText: 'DO you think you are dumb?',
+			answerOptions: [
+				{ answerText: 'Definetly', isCorrect: false,rating:0 },
+				{ answerText: 'I think Soo', isCorrect: false,rating:1 },
+				{ answerText: "NO, I don't think soo", isCorrect: true,rating:2 },
+				{ answerText: 'NO, fuck you', isCorrect: false,rating:3 },
+			],
+		},
+        {
+			questionText: 'DO you think you are dumb?',
+			answerOptions: [
+				{ answerText: 'Definetly', isCorrect: false,rating:0 },
+				{ answerText: 'I think Soo', isCorrect: false,rating:1 },
+				{ answerText: "NO, I don't think soo", isCorrect: true,rating:2 },
+				{ answerText: 'NO, fuck you', isCorrect: false,rating:3 },
+			],
+		},
+        {
+			questionText: 'DO you think you are dumb?',
+			answerOptions: [
+				{ answerText: 'Definetly', isCorrect: false,rating:0 },
+				{ answerText: 'I think Soo', isCorrect: false,rating:1 },
+				{ answerText: "NO, I don't think soo", isCorrect: true,rating:2 },
+				{ answerText: 'NO, fuck you', isCorrect: false,rating:3 },
+			],
+		},
+        {
+			questionText: 'DO you think you are dumb?',
+			answerOptions: [
+				{ answerText: 'Definetly', isCorrect: false,rating:0 },
+				{ answerText: 'I think Soo', isCorrect: false,rating:1 },
+				{ answerText: "NO, I don't think soo", isCorrect: true,rating:2 },
+				{ answerText: 'NO, fuck you', isCorrect: false,rating:3 },
+			],
+		},
+        {
+			questionText: 'DO you think you are dumb?',
+			answerOptions: [
+				{ answerText: 'Definetly', isCorrect: false,rating:0 },
+				{ answerText: 'I think Soo', isCorrect: false,rating:1 },
+				{ answerText: "NO, I don't think soo", isCorrect: true,rating:2 },
+				{ answerText: 'NO, fuck you', isCorrect: false,rating:3 },
+			],
+		},
+        {
+			questionText: 'DO you think you are dumb?',
+			answerOptions: [
+				{ answerText: 'Definetly', isCorrect: false,rating:0 },
+				{ answerText: 'I think Soo', isCorrect: false,rating:1 },
+				{ answerText: "NO, I don't think soo", isCorrect: true,rating:2 },
+				{ answerText: 'NO, fuck you', isCorrect: false,rating:3 },
+			],
+		},
+        {
+			questionText: 'DO you think you are dumb?',
+			answerOptions: [
+				{ answerText: 'Definetly', isCorrect: false,rating:0 },
+				{ answerText: 'I think Soo', isCorrect: false,rating:1 },
+				{ answerText: "NO, I don't think soo", isCorrect: true,rating:2 },
+				{ answerText: 'NO, fuck you', isCorrect: false,rating:3 },
+			],
+		},
+	];
+
+export default questions;
+
